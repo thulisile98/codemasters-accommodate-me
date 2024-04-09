c36f9a15372fd6894752c917897c2e34f8323377
Angel Mashego <thulisile.ma98@gmail.com>
2024-04-09 11:22:23 +0200

    iinitial commit

---
+++ src/main/java/com/codemasters/accommodateme/AccommodateMeApplication.java
@@ -0,0 +1,13 @@
+package com.codemasters.accommodateme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommodateMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccommodateMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codemasters/accommodateme/AccommodateMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemasters.accommodateme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodateMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
